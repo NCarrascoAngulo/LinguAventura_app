7fffda86c4e62c28093cabb504822e5e462299fb
Nicol <123992698+NCarrascoAngulo@users.noreply.github.com>
2024-12-09 06:37:57 -0500

    Update __init__.py

--- capp/__init__.py
+++ capp/__init__.py
@@ -6,17 +6,25 @@ import os
 
 application = Flask(__name__)
 
-### Code GitHub
+### Secret Key GitHub
 # application.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 
-### Code Computer
-# application.config['SECRET_KEY'] = '3oueqkfdfas8ruewqndr8ewrewrouewrere44554'
+### Secret Key Computer
+application.config['SECRET_KEY'] = '3oueqkfdfas8ruewqndr8ewrewrouewrere44554'
 
-# Base de datos
-# DBVAR = 'sqlite:///user.db'
-# application.config['SQLALCHEMY_DATABASE_URI'] = DBVAR
-# application.config['SQLALCHEMY_BINDS'] ={'sentence': 'sqlite:///sentence_table.db'}
-# db = SQLAlchemy(application)
+# Base de datos GitHub
+DBVAR = f"postgresql://{os.environ['RDS_USERNAME']}:{os.environ['RDS_PASSWORD']}@{os.environ['RDS_HOSTNAME']}/{os.environ['RDS_DB_NAME']}"
+DBVAR = 'postgresql://username:password@endpoint:5432/ebdb'
+application.config['SQLALCHEMY_DATABASE_URI'] = DBVAR 
+application.config['SQLALCHEMY_BINDS'] ={'sentence': DBVAR}
+db = SQLAlchemy(application)
+
+
+# Base de datos ordenador
+DBVAR = 'sqlite:///user.db'
+application.config['SQLALCHEMY_DATABASE_URI'] = DBVAR
+application.config['SQLALCHEMY_BINDS'] ={'sentence': 'sqlite:///sentence_table.db'}
+db = SQLAlchemy(application)
 
 # Bcrypt
 bcrypt = Bcrypt(application)
@@ -35,4 +43,3 @@ application.register_blueprint(home)
 application.register_blueprint(business)
 application.register_blueprint(light_talk_app)
 application.register_blueprint(users)
-

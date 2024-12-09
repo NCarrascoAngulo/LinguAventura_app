46e0a5eb465087f4f9bb35156385a9fe2db47ce6
Nicol <123992698+NCarrascoAngulo@users.noreply.github.com>
2024-12-09 05:07:37 -0500

    Update __init__.py

--- capp/__init__.py
+++ capp/__init__.py
@@ -1,38 +1,13 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
 import os
 
 application = Flask(__name__)
 
-### Secret Key GitHub
+### Code GitHub
 # application.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 
-### Secret Key Computer
+### Code Computer
 application.config['SECRET_KEY'] = '3oueqkfdfas8ruewqndr8ewrewrouewrere44554'
-
-# Base de datos GitHub
-# DBVAR = f"postgresql://{os.environ['RDS_USERNAME']}:{os.environ['RDS_PASSWORD']}@{os.environ['RDS_HOSTNAME']}/{os.environ['RDS_DB_NAME']}"
-DBVAR = 'postgresql://postgress:ugWQXDdLZ0RgNZ0gIVf4@awseb-e-ixaw9imbh6-stack-awsebrdsdatabase-mb7kcmwiy2a1.cn480w84mbrc.us-east-1.rds.amazonaws.com:5432/ebdb'
-application.config['SQLALCHEMY_DATABASE_URI'] = DBVAR 
-application.config['SQLALCHEMY_BINDS'] ={'sentence': DBVAR}
-db = SQLAlchemy(application)
-
-
-# # Base de datos ordenador
-# DBVAR = 'sqlite:///user.db'
-# application.config['SQLALCHEMY_DATABASE_URI'] = DBVAR
-# application.config['SQLALCHEMY_BINDS'] ={'sentence': 'sqlite:///sentence_table.db'}
-# db = SQLAlchemy(application)
-
-# Bcrypt
-bcrypt = Bcrypt(application)
-
-# Login
-login_manager= LoginManager(application)
-login_manager.login_view = 'users.login'
-login_manager.login_message_category = 'info'
 
 from capp.home.routes import home
 from capp.business.routes import business
@@ -43,4 +18,5 @@ application.register_blueprint(home)
 application.register_blueprint(business)
 application.register_blueprint(light_talk_app)
 application.register_blueprint(users)
+
 
